6e27da912c21099b520c4f1edb1fe9d342ac99f6
Anoushka <anoushka19c@gmail.com>
2024-08-05 12:54:16 +0530

    ..

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
-# main.py
+import sys
+from pathlib import Path
+
+# Ensure the project root is in the PYTHONPATH
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
 
 import streamlit as st
 from app.components.sidebar import sidebar
