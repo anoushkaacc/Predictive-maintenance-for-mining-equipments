bbdea3de8f3541b4da190cabea448cdda648ce46
Anoushka <anoushka19c@gmail.com>
2024-08-06 12:23:13 +0530

    ok

---
+++ app/components/__init__.py
@@ -0,0 +1,3 @@
+from ..data_loader import load_data
+from ..data_visualization import plot_feature_correlations
+PACKAGE_NAME = "components_script"
--- app/components/main_view.py
+++ app/components/main_view.py
@@ -1,6 +1,12 @@
 import streamlit as st
-from app.data_loader import load_data
-from app.data_visualization import plot_feature_correlations
+from data_loader import load_data
+from data_visualization import plot_feature_correlations
+
+def main():
+    load_data()
+    plot_feature_correlations()
+
+
 def main_view(data_file):
     if data_file:
         data = load_data(data_file)
